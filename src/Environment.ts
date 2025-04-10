8eaa973abbc7995f8404fb944c04f68c66c045e2
Kristo Jorgenson <kristo.jorgenson@gmail.com>
2025-04-10 11:58:57 +0200

    remove some env vars

--- src/Environment.ts
+++ src/Environment.ts
@@ -1,27 +1,25 @@
-import { Label } from "@/schema"
-import { Config, Effect } from "effect"
+import { Label } from "@/schema";
+import { Config, Effect } from "effect";
 
 export class Env extends Effect.Service<Env>()("Env", {
   accessors: true,
-  effect: Effect.gen(function*() {
-    const labelerSocketUrl = yield* Config.url("LABELER_SOCKET_URL")
-    const bskyService = yield* Config.url("BSKY_SERVICE")
-    const labelerDid = yield* Config.nonEmptyString("LABELER_DID")
-    const labelerPassword = yield* Config.nonEmptyString("LABELER_APP_PASSWORD")
+  effect: Effect.gen(function* () {
+    const labelerSocketUrl = yield* Config.url("LABELER_SOCKET_URL");
+    const bskyService = yield* Config.url("BSKY_SERVICE");
     const labelerCursorFilepath = yield* Config.nonEmptyString(
-      "LABELER_CURSOR_FILEPATH",
-    ).pipe(Config.withDefault("cursor.txt"))
+      "LABELER_CURSOR_FILEPATH"
+    ).pipe(Config.withDefault("cursor.txt"));
     // reverse the list because bluesky orders them based on latest creation
-    const labelsToList = yield* Config.array(Config.string(), "LABELS_TO_LIST")
-      .pipe(Effect.map((v) => v.reverse().map((l) => Label.make(l))))
+    const labelsToList = yield* Config.array(
+      Config.string(),
+      "LABELS_TO_LIST"
+    ).pipe(Effect.map((v) => v.reverse().map((l) => Label.make(l))));
 
     return {
       labelerSocketUrl,
       bskyService,
-      labelerDid,
-      labelerPassword,
       labelerCursorFilepath,
       labelsToList,
-    }
+    };
   }),
 }) {}
